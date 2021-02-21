9cac1339a58430f5937b47976701753a564a6056
Raveendra Panta <raveendra@naphix.com.au>
2021-02-21 18:55:46 +0545

    onClick() event

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,15 +4,21 @@ import { useState } from "react";
 function Home() {
     const [name, setName] = useState('Shyam');
 
-    const handleClick = () => {
-        setName('Ram');
+    const handleClick = (event, data) => {
+        console.log(event);
+        setName(data.name);
     }
 
     return (
         <div>
             <h1>Home page</h1>
             <p>{ name }</p>
-            <button onClick={handleClick}>Click Me</button>
+            <button onClick={
+                (event) => handleClick(event, {
+                    name: (new Date()).toString(),
+                    age: 31
+                })
+            }>Click Me</button>
         </div>
     )
 }
